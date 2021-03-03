0863946917cc3b228f8ed0437d1afaf07d9d0772
RaimondaRA <raimonda.alisauskaite@gmail.com>
2021-03-03 17:33:29 +0200

    Pridėta prisijungimo lango GUI.

--- app/src/main/java/com/example/corona_2021/LoginActivity.java
+++ app/src/main/java/com/example/corona_2021/LoginActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText username, password;
@@ -19,33 +22,43 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText username = (EditText) findViewById(R.id.username);
-        EditText password = (EditText) findViewById(R.id.password);
-
-        Button login = (Button) findViewById(R.id.login);
+        final EditText username = findViewById(R.id.username);
+        final EditText password = findViewById(R.id.password);
+        Button login = findViewById(R.id.login);
 
         login.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            @Override
+            public void onClick(View focusView) {
 
-                Toast.makeText(LoginActivity.this, "Button clicked", Toast.LENGTH_LONG).show();
-                //Perform action on click
-                Intent activityChangeIntent = new Intent(LoginActivity.this, NextActivity.clone);
-                startActivity (activityChangeIntent);
-            }
-        });
-
-        Button register = (Button) findViewById(R.id.register);
-
-        register.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                Toast.makeText(LoginActivity.this, "Button clicked", Toast.LENGTH_LONG).show();
+                String username2 = username.getText().toString();
+                String password2 = password.getText().toString();
+                Toast.makeText(LoginActivity.this, "Username " + username2 + "\n" + "Password " + password2, Toast.LENGTH_SHORT).show();
 
                 //Perform action on click
-                //Intent activityChangeIntent = new Intent(LoginActivity.this, NextActivity.clone);
-                //startActivity (activityChangeIntent);
+                Intent goToSearchActivity = new Intent(LoginActivity.this, SearchActivity.class);
+                startActivity(goToSearchActivity);
+                //Klaidu zurnalo isvalymas
+                username.setError(null);
+                password.setError(null);
+                if (Validation.isCredentialsValid(username2) && Validation.isCredentialsValid(password2)){
+
+                    goToSearchActivity = new Intent(LoginActivity.this, SearchActivity.class);
+                    startActivity(goToSearchActivity);
+            } else {
+                    username.setError(getResources().getString(R.string.login_invalid_credentials_message));
+                    username.requestFocus();
+                }
             }
         });
+    }
+}
+
+
+
+
+
+
+
 
 
 

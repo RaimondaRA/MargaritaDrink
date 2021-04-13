342b12df6d4b13641c4ed4b58c45668ae3f08a1b
RaimondaRA <raimonda.alisauskaite@gmail.com>
2021-04-13 15:50:23 +0300

    Įrašų paieška iš JSON ir atvaizdavimas GUI. T06 1 dalis

--- app/src/main/java/com/example/corona_2021/JSON.java
+++ app/src/main/java/com/example/corona_2021/JSON.java
@@ -7,11 +7,13 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class JSON {
+public class JSON { //is URL istraukiamas JSON ir jis atspausdinamas
 
     private static String readAll(Reader rd) throws IOException { //metodas
         StringBuilder sb = new StringBuilder();
@@ -21,7 +23,8 @@ public class JSON {
         }
         return sb.toString();
     }
-    //Visas ilgas tekstas is API nuskaitomas i stringa, o is tringo jis konvertuojamas i JSON objekta
+
+    //Visas ilgas tekstas is API nuskaitomas i stringa, o is stringo jis konvertuojamas i JSON objekta
     public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException { //metodas. Kreipsimes i URL ir mums is jo grazins JSON
         InputStream is = new URL(url).openStream();
         try { // i try talpinamas kodas, kuriame gali kilti kokia nors isimtis - klaida
@@ -32,6 +35,52 @@ public class JSON {
         } finally { //Ivyks ar neivyks klaida, galiausiai viskas bus uzdaryta. Siuo atveju uzdaromi ivedimo srautai
             is.close();
         }
+    }
+
+    //metodas, kuris paims JSON objekta ir grazins masyva. Issitraukiame is JSON tik tai, kas mus domina. Nereikalingos info reikia atsikratyti. Isimsim pacia pradzia (meta duomenis) ir pabaigos simbolius
+    public static ArrayList<Corona> getList(JSONArray jsonArray) throws JSONException {//metodo antraste. Grazins sarasa, Corona klases objektu (ArrayList, o ne visa JSON). JSONArray yra klase ir jsonArray yra objektas. ArrayList - vienas is sarasu programavime
+        ArrayList<Corona> coronaList = new ArrayList<Corona>();//sukureme sarasa, kur norime pataplinti klases Corona objektus. coronaList - saraso pavadinimas.
+        //isimti "data" is JSON (pirma eilute) ir issaugoti corona objektu sarase (coronaList)
+        for (int i = 0; i < jsonArray.length(); i++) { //ciklas, rodo 3 salygas: pradinis iteratorius i
+            JSONObject jsonObject = jsonArray.getJSONObject(i); //eisim per visa sarasa JSON masyvo
+            Corona corona = new Corona( //Corona konstruktorius, susideda is 6 elemntu. 1 elementas - country, todel trauksime country
+                    //public Corona(String country, String lastUpdate, String keyID, int confirmed, int deaths)
+                    jsonObject.getString("country"), //country is mazosios, nes JSON duomenyse irgi is mazosios. Traukiant raktus is JSON, jie turi buti IDENTISKI
+                    jsonObject.getString("lastUpdate"),
+                    jsonObject.getString("keyId"),
+                    jsonObject.getInt("confirmed"), //visi raktai yra String tipo, eilutes, nors ir grazins int skaiciu
+                    jsonObject.getInt("deaths")
+            );
+            coronaList.add(corona); //eis per visus JSON sarase esancius objektus, paims objektus, issitrauks reiksmes, konstruosime corona klases objekta ir prideime i corona sarasa
+        }
+
+        return coronaList;
+    }
+
+    public static JSONArray getJSONArray(JSONObject jsonObject) throws JSONException {//metodas
+        //pasalinama is JSON visa nereikalinga info (meta duomenys), paliekant tik covid19Stats masyva
+        int jsonLength = jsonObject.toString().length(); //mums reikia tik covid19Stats. Gauname viso JSON ilgi (apie 80 tukst simboliu), grazina ilgio skaiciu
+        String covid19Stats = "{" + jsonObject.toString().substring(96, jsonLength) + "}"; //jsonObject konvertuojame i eilute (String), substring - iskerpa dali simboliu is stringo. Iskirps nuo 96-to iki pacio galo
+
+        //String konvertacija i JSON objekta
+        JSONObject jsonObject1 = new JSONObject(covid19Stats); //perduodame covid19Stats stringa
+
+        //JSONObject i JSONArray. Sukonstarvome is objekto sarasa, masyva
+        JSONArray jsonArray = jsonObject1.getJSONArray("covid19Stats");
+        return jsonArray; //jsonArray paims getList
+    }
+
+    //pagal visa sarasa suformuos tik tra valstybe, kurios mumsreikia
+    public static ArrayList<Corona> getCoronaListByCountry(ArrayList<Corona> coronaArrayList, String country) { //metodas, noresim gauti sarasa pagal valstybe. F-ja paims du parametrus - ArrayList, valstybes pav. Grazins arrayList
+        ArrayList<Corona> coronaListByCountry = new ArrayList<Corona>();
+        //pereisime per visa sarasa Corona ArrayList, ieskosime tos valstybes ir formuosime
+        for (Corona corona : coronaArrayList) { //desineje puseje bus sukuriamas tos klases objektas, per kurios sarasa iteruojame. Iteruojame per klases objektus
+            if (corona.getKeyId().contains(country)) { //contains metodas (vienas is String metodu) - iesko zodzio dalies. Pradejus rasyti zodi, pradeda ieskoti.
+                coronaListByCountry.add(corona);
+            }
+        }
+
+        return coronaListByCountry;
     }
 }
 
--- app/src/main/java/com/example/corona_2021/SearchActivity.java
+++ app/src/main/java/com/example/corona_2021/SearchActivity.java
@@ -7,14 +7,17 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity { //cia yra globalus kintamieji, kurie aprasomi klases virsuje
     public static final String COVID_API = "https://covid19-api.weedmark.systems/api/v1/stats"; //kreipiames i URL, kad duotu si API
+    private ArrayList <Corona> coronaList = new ArrayList <Corona>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +25,10 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         //Bus paleidziama nauja gija (procesas) - nuskaitymui JSON is API
-        AsyncFetch asyncFetch = new AsyncFetch(); //Sukuriame AsuncFetch klase
-        asyncFetch.execute();
+        AsyncFetch asyncFetch = new AsyncFetch(); //Sukuriame AsyncFetch klase
+        asyncFetch.execute(); //execute iskviecia metodus toje klaseje sukurtoje (t. y. AsyncFetch)
     }
-
+    //klase AsyncFetch naudojame todel, kad bus du lygiagretus procesai (uzduotys). AsyncFetch turi tuos metodus, reikalingus lygiagreciam keliu procesu veikimui
     private class AsyncFetch extends AsyncTask<String, String, JSONObject> {//klase bus naudojama tik sios AsyncFetch klases viduje. Extends - prapleciame tevine klase AsyncTask, kuri atsakinga uz lygiagretuma (keliu procesu veikima vienu metu)
         ProgressDialog progressDialog = new ProgressDialog(SearchActivity.this); //nurodome, kur jis suksis, t.y. SearchActivity lange
 
@@ -37,13 +40,13 @@ public class SearchActivity extends AppCompatActivity {
             progressDialog.show(); //bus rodomas besisukantis dialogas
         }
 
-        @Override
+        @Override //vyksta antras procesas tuo paciu metu, kai sukasi rutuliukas ekrane. Fone - kreipiames i URL, gauname duomenis
         //Sis metodas skirtas gavimui JSON is API
         protected JSONObject doInBackground(String... strings) { //Sis metodas bus vykdomas tuo metu, kai vartotojas matys besisukanti dialoga (progressDialog)
             try { //try yra blogas, kuriame gali kilti klaidu, pvz. nepavyks nuskaityti JSON. Tai, kas ivyks, bus apdorojama catch'u - isimtyse. Programa neuzlus, o isves vartotojui tam tikrus pranesimus
-                JSONObject jsonObject = JSON.readJsonFromUrl(COVID_API); //Kreipsimes i klase Json, kuria nusikopijavome. Perduosime URL, kuris yra globalus kintamasis, public tipo
+                JSONObject jsonObject = JSON.readJsonFromUrl(COVID_API); //Kreipsimes i klase Json.java, kuria nusikopijavome. Perduosime URL, kuris yra globalus kintamasis, public tipo
                 return jsonObject; //Jei viskas gerai, grazinsime jsonObject
-            } catch (IOException e) { //
+            } catch (IOException e) { //input output exception - ivedimo, isvedimo isimtys
                 Toast.makeText(SearchActivity.this,
                         getResources().getString(R.string.search_error_reading_data) + e.getMessage(), //ismesime pranesima apie error'a, getMessage - pateiksime papildomos info
                         Toast.LENGTH_LONG
@@ -61,7 +64,7 @@ public class SearchActivity extends AppCompatActivity {
         protected void onPostExecute(JSONObject jsonObject) { //onPostExecute - padaryk kazka po. Execute atitinka doInBackground
             progressDialog.dismiss(); //gavome duomenis is background, todel panaikiname besisukanti dialoga
 
-            int statusCode = 0;
+            int statusCode = 0; //kadangi statusCode nezinome, tai irasome 0, pradine reiksme. Virsuj aprasome try
             try { //Kai yra JSON, visada bus try-catch, nes su JSON gali buti klaidu
                 statusCode = jsonObject.getInt("statusCode"); //Bandoma gauti statusCode (HTTP busenos koda), kuris bus grazinamas is JSON. Jei bus problemu - catch
             } catch (JSONException e) {
@@ -72,15 +75,26 @@ public class SearchActivity extends AppCompatActivity {
             } //baigiasi catch
 
             if(statusCode == HttpURLConnection.HTTP_OK){ //Jei viskas OK, galime tiketis JSON
-                System.err.println(jsonObject.toString()); //Atspausdins ilga JSON teksta (valstybiu sarasa). err - spausdins kaip klaida, kitokios spalvos. System.err - galime pasiziureti teksta terminale
-                Toast.makeText(SearchActivity.this, jsonObject.toString(), //Isspausdins vartotojui JSON, jei viskas gerai
-                        Toast.LENGTH_LONG
-                ).show();
+                //System.err.println(jsonObject.toString()); //Atspausdins ilga JSON teksta (valstybiu sarasa). err - spausdins kaip klaida, kitokios spalvos. System.err - galime pasiziureti teksta terminale
+                //Toast.makeText(SearchActivity.this, jsonObject.toString(), //Isspausdins vartotojui JSON, jei viskas gerai
+                        //Toast.LENGTH_LONG
+                //).show();
+
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = JSON.getJSONArray(jsonObject); //is JSON object suformuoja JSON Array
+                    coronaList = JSON.getList(jsonArray);
+                    System.out.println("Italy covidStats:" + JSON.getCoronaListByCountry(coronaList, "Italy")); //grazins sarasa
+                } catch (JSONException e) {
+                    //e.printStackTrace();
+                    System.out.println(getResources().getString(R.string.search_error_reading_data) + e.getMessage());
+                }
+
             } else { // Kazkas nepavyko (serveris negrazino kodo 200 (OK))
                 String message = null;
                 try {
                     message = jsonObject.getString("message");
-                } catch (JSONException e) { //zemiau apdoroma zinute JSON exeption'ui
+                } catch (JSONException e) { //zemiau apdoroma zinute JSON exeption'ui. Catch pagauna klaida ir ja isspausdina (pvz. per Toast)
                     Toast.makeText(SearchActivity.this,
                             getResources().getString(R.string.search_error_reading_data) + e.getMessage(), //ismesime pranesima apie error'a, getMessage - pateiksime papildomos info
                             Toast.LENGTH_LONG
